28577cdd1036f7470f00136e67512921075227fb
jijiangrui <379369845@qq.com>
2024-07-23 11:09:25 +0800

    feat: add douban

--- navigation/url.js
+++ navigation/url.js
@@ -58,6 +58,7 @@ function myFunction() {
             items: [
                 { href: 'https://weread.qq.com/', icon: 'icon-self-weixindushu', text: '微信读书' },
                 { href: 'https://ankiweb.net/', icon: 'icon-self-anki', text: 'Anki' },
+                { href: 'https://book.douban.com/https://book.douban.com/', icon: 'icon-self-douban', text: '豆瓣读书' },
                 { href: 'https://filehelper.weixin.qq.com/', icon: 'icon-self-weixinwenjian', text: '文件传输助手' },
             ]
         },
